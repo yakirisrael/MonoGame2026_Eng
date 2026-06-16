4c5116b64b5a7d5a5c835707f140e48107f6db66
Your Name <yakir.israel1@gmail.com>
2026-06-16 15:37:08 +0300

    Monogame first project

---
+++ MonoGame2026/Game1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGame2026;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    private Texture2D _logo;
+
+    private Vector2 ScreenCenter;
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+        _graphics.IsFullScreen = true;
+        _graphics.PreferredBackBufferWidth = 1920;
+        _graphics.PreferredBackBufferHeight = 1080;
+
+        ScreenCenter = new Vector2(_graphics.PreferredBackBufferWidth * 0.5f,
+                                   _graphics.PreferredBackBufferHeight * 0.5f);
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        
+        _logo =  Content.Load<Texture2D>("Images/logo");
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+            Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.DarkRed);
+
+        
+        _spriteBatch.Begin();
+
+        Rectangle DestRect = new Rectangle(
+            new Point((int)ScreenCenter.X, (int)ScreenCenter.Y),
+            new Point(_logo.Width, _logo.Height));
+        
+        _spriteBatch.Draw(
+            _logo, 
+            DestRect, 
+            null,
+            Color.White,
+            MathHelper.ToRadians(0),
+            new Vector2(_logo.Width * 0.5f, _logo.Height * 0.5f),
+            SpriteEffects.None,
+            0
+            );
+        
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
---
+++ MonoGame2026/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new MonoGame2026.Game1();
+game.Run();
